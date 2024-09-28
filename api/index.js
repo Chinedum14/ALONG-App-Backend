549852913c1635014f92e21a2c9edf977c6c8701
Chinedum14 <chinedumnwankwo18@gmail.com>
2024-09-28 13:31:08 +0100

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import './db/database.js';
+
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+const app = express();
+
+const corsOptions = {
+  origin: ['https://along-preview.netlify.app', 'http://localhost:5173'],
+  credentials: true,
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+
+
+app.listen(port, () => {
+  console.log('App is running on port: ', port);
+});
