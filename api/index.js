4cbdbd9e9af792330e2aa8717da4136e6afbcfc3
Chinedum14 <chinedumnwankwo18@gmail.com>
2024-09-28 13:38:28 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,7 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import './db/database.js';
-
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import "./db/database.js";
 
 dotenv.config();
 
@@ -11,14 +10,13 @@ const port = process.env.PORT;
 const app = express();
 
 const corsOptions = {
-  origin: ['https://along-preview.netlify.app', 'http://localhost:5173'],
+  origin: ["https://along-preview.netlify.app", "http://localhost:5173"],
   credentials: true,
 };
 
 app.use(express.json());
 app.use(cors(corsOptions));
 
-
 app.listen(port, () => {
-  console.log('App is running on port: ', port);
+  console.log("App is running on port: ", port);
 });
